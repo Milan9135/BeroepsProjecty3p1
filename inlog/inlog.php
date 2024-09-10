db452d23cbc9676f2531275afff36497dc124f5b
itsnotlv <146447633+itsnotlv@users.noreply.github.com>
2024-09-10 12:30:39 +0200

    werpff

--- inlog/inlog.php
+++ inlog/inlog.php
@@ -1,13 +1,42 @@
+<?php
+$host = "localhost";
+$dbname = "Tandartsdb";
+$username = "root";
+$password = "";
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$naam = "Lebron james";
+$geboortedatum = "1980-05-15";
+$email = "lebronjamse@gmail.com";
+$telefoonnummer = "0612345678";
+$adres = "123 Main Street, Amsterdam";
+$beoordeling = 4.8;
+$specialisaties = "Orthodontist, lEdental";
+$beschrijving = "Experienced dentist specializing in orthodontics and cosmetic dentistry.";
+
+$sql = "INSERT INTO Tandarts (Naam, Geboortedarum, Telefoonnummer, Adres, Beoordeling, Specialisaties, Beschrijving)
+        VALUES ('$naam', '$geboortedatum', '$telefoonnummer', '$adres', '$beoordeling', '$specialisaties', '$beschrijving')";
+
+if ($conn->query($sql) !== TRUE) {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+
 <!DOCTYPE html>
 <html lang="nl">
-
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="tand.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;600&display=swap" rel="stylesheet">
 </head>
-
 <body>
     <nav class="navbar">
         <a href="./indexs.html/">Home</a>
@@ -39,5 +68,4 @@
 
     <script src="script.js"></script>
 </body>
-
 </html>

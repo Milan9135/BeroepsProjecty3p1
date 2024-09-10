f9e02f54b6eb97856cddc414ffffb3821c7e4101
Milan9135 <Milpower0@gmail.com>
2024-09-10 12:28:22 +0200

    removed duplicate db sql and fixed some broken references

--- register.php
+++ register.php
@@ -27,7 +27,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Registreren</title>
-    <link rel="stylesheet" href="register.css" />
+    <link rel="stylesheet" href="styles/register.css" />
 </head>
 
 <body>
